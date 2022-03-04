64a79c191b081bc0e655262c7cb57b6bce5fbbdd
cristianofmc <cristiano.fmc@hotmail.com>
2022-03-03 22:49:28 -0300

    added tailwindcss support

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import Link from 'next/link';
 
 function Home(){
-    return <div>
+    return <div className='bg-slate-800'>
         <h1>Selections - teste</h1>
         <Link href="/about"><a>Sobre</a></Link>
+        <h1 className="text-orange-600 text-3xl">HEllo</h1>
     </div>
 }
 
