4216c217ab994e7496e1f89d2e1e2972b735e394
cristianofmc <cristiano.fmc@hotmail.com>
2022-03-04 21:38:44 -0300

    added more files

---
+++ components/Hero.js
@@ -0,0 +1,12 @@
+const Hero = () => {
+    return(
+        <div>
+            <div class="relative max-w-5xl mx-auto pt-20 sm:pt-24 lg:pt-52">
+                <h1 class="text-zinc-800 font-extrabold text-5xl xs:text-4xl lg:text-7xl tracking-tight text-center dark:text-white">Cristiano Felipe</h1>
+            </div>
+            <div className="text-center pt-10 text-xl">selections are emerging</div>
+        </div>
+    );
+};
+
+export default Hero;
--- pages/about.js
+++ pages/about.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 function About(){
     return <div>
-        <h1>Selections - teste</h1>
+        <h1 className='text-gray-600'>Selections - teste</h1>
         <Link href="/"><a>Home</a></Link>
     </div>
 }
