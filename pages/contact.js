fde35e32a4fe722e8a841be719144b03b11fb742
cristianofmc <cristiano.fmc@hotmail.com>
2022-03-04 22:27:16 -0300

    updated contact file

---
+++ pages/contact.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+function Contact(){
+    return <div>
+        <h1 className='text-gray-600'>Selections - teste</h1>
+        <Link href="/"><a>Home</a></Link>
+    </div>
+}
+
+export default Contact
