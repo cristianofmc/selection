4a5353e824bb091c6be7097bc1e99ee34242b71a
cristianofmc <cristiano.fmc@hotmail.com>
2022-03-04 22:23:46 -0300

    updated eslin

--- components/Hero.js
+++ components/Hero.js
@@ -2,7 +2,7 @@ const Hero = () => {
     return(
         <div>
             <div class="relative max-w-5xl mx-auto pt-20 sm:pt-24 lg:pt-52">
-                <h1 class="text-zinc-800 font-extrabold text-5xl xs:text-4xl lg:text-7xl tracking-tight text-center dark:text-white">Cristiano Felipe</h1>
+                <h1 className="text-zinc-800 font-extrabold text-5xl xs:text-4xl lg:text-7xl tracking-tight text-center dark:text-white">Cristiano Felipe</h1>
             </div>
             <div className="text-center pt-10 text-xl">selections are emerging</div>
         </div>
