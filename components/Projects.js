dc30de8673f445f56e6ad194ccbd71f8d2a21096
cristianofmc <cristiano.fmc@hotmail.com>
2022-06-07 23:23:19 -0300

    added load image support

--- components/Projects.js
+++ components/Projects.js
@@ -1,12 +1,18 @@
 const Projects = (props) => {
+    
     return(
         <div id="projects" className="pt-3">
             <ul key="1" className="mx-4 grid sm:grid-cols-2 md:grid-cols-4  gap-y-10 gap-x-6">
                 {
-                    props.children.map((data) => {
+                    
+                    props.children.map((data, index) => {
+                        
+                        let cover_url = data.html_url.replace("https://github.com/", "https://raw.githubusercontent.com/");
+                        cover_url += `/${data.default_branch}/_docs/cover.webp`;
+
                         return  <li key={data.id} className="mx-2 rounded-lg border border-zinc-500">
                                     <div className="overflow-hidden ">
-                                        <img className="relative w-full h-40 object-cover rounded-t-md" src="https://tm.ibxk.com.br/2021/03/08/08105041400097.jpg?ims=1200x675"></img>
+                                        <img className="relative w-full h-40 object-cover rounded-t-md" alt={data.name} src={cover_url} width='600' height='400' onError={(event) => event.target.style.display = 'none'}></img>
                                     </div>
                                     <div className="px-4 pt-3">
                                         <h1 className="font-bold">{data.name}</h1>
