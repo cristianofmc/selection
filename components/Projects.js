738bc939bf169dfd72e8baa7932b355542dfaf98
cristiano.fmc <cristiano.fmc@hotmail.com>
2022-07-05 21:28:33 -0300

    added simple tags

--- components/Projects.js
+++ components/Projects.js
@@ -11,7 +11,6 @@ const Projects = (props) => {
 
                         let cover_url = data.html_url.replace("https://github.com/", "https://raw.githubusercontent.com/");
                         cover_url += `/${data.default_branch}/_docs/cover.webp`;
-
                         return <li key={data.id} className="mx-2 rounded-lg border border-zinc-500">
 
                             <div className="overflow-hidden">
@@ -22,14 +21,13 @@ const Projects = (props) => {
                                 <h1 className="font-bold">{data.name}</h1>
                                 <p>{data.description}</p>
                             </div>
-
-                            {data.topics.map((tag) => {
-                                <div className="px-2.5 pt-1.5 pb-3.5">
-                                    <a className="border border-zinc-500 inline-flex items-center h-6 rounded-full text-sm whitespace-nowrap px-2 mr-1.5 mt-1.5" href="/">
-                                        {tag}
-                                    </a>
-                                </div>
-                            })}
+                            <div className="px-4 pt-3">
+                                {data.topics.map((tag) => (
+                                    <div className="px-2.5 pt-1.5 pb-3.5" key={`${data.id}_${tag}`}>
+                                        <a className="border border-zinc-500 inline-flex items-center h-6 rounded-full text-sm whitespace-nowrap px-2 mr-1.5 mt-1.5" href="/">{tag}</a>
+                                    </div>
+                                ))}
+                            </div>
 
                         </li>
                     })
