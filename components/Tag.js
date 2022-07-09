7f842f43bb04556789f89e53fd9775ea0d553956
cristiano.fmc <cristiano.fmc@hotmail.com>
2022-07-09 17:57:39 -0300

    removed comments

--- components/Tag.js
+++ components/Tag.js
@@ -1,11 +1,8 @@
 const Tag = (props) => {
-    console.log(props)
-    console.log(props.childen);
     return(
         <div className="px-2.5 pt-1.5 pb-3.5">
 
             {props.children.map((tag) => (
-
                 <a className="border border-zinc-500 inline-flex items-center h-6 rounded-full text-sm whitespace-nowrap px-2 mr-1.5 mt-1.5" href="/">{tag}</a>
             ))
             }
