c25b3c7bc048fdb27410d7ebe2c81da9daa7fc70
cristiano.fmc <cristiano.fmc@hotmail.com>
2022-07-22 18:46:08 -0300

    I modified some pages to adapt to bio content from github

--- components/MenuItem.js
+++ components/MenuItem.js
@@ -1,9 +1,12 @@
+import Link from 'next/link';
+
 const MenuItem = (props) => {
     return(
-        <a class="group inline-flex items-center px-5 md:text-md  md:h-10 h-7 text-sm rounded-full font-semibold whitespace-nowrap focus:outline-none focus:ring-2 bg-neutral-100 mt-8"
-        href={`/${props.href}`}>
-        {props.children}
-        </a>
+        <Link href={`/${props.href}`}>
+            <a class="group inline-flex items-center px-5 md:text-md  md:h-10 h-7 text-sm rounded-full font-semibold whitespace-nowrap focus:outline-none focus:ring-2 bg-neutral-100 mt-8">
+                {props.children}
+            </a>
+        </Link>
     );
 };
 
--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,37 @@
-import Link from 'next/link';
+import Head from 'next/head';
+import Hero from '../components/Hero';
+import MenuItem from '../components/MenuItem';
+import { getUserDetails, getReposByTopics} from '../lib/github-api';
 
-function About(){
-    return <div>
-        <h1 className='text-gray-600'>Selections - teste</h1>
-        <Link href="/"><a>Home</a></Link>
-    </div>
+
+function About(props){
+    return(
+        <div>
+            <Head><title>{props.user.name}-About</title></Head>
+            <Hero>{props.user.name}</Hero>
+            <div class="mt-6 sm:mt-10 flex justify-center space-x-6 ">
+                <MenuItem href="">Projects</MenuItem>
+                <MenuItem href="about">About</MenuItem>
+                <MenuItem href="contact">Contact</MenuItem>
+            </div>
+            <section className="mt-16 max-w-7xl mx-auto sm:px-6 md:px-8">
+                <div id="about" className="pt-3">
+                    <img className="rounded-full" src={props.user.avatar_url}></img>
+                    <p>{props.user.bio}</p></div>
+            </section>
+        </div>
+    )
+}
+
+export async function getStaticProps(){
+    const userDetails = await getUserDetails()
+     console.log(userDetails)
+    return {
+        props: {
+            user:userDetails
+        },
+        revalidate: 1
+    }
 }
 
 export default About
