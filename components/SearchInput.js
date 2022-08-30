ef9ce929c338e9a38166b992726f9708246a8447
cristiano.fmc <cristiano.fmc@hotmail.com>
2022-08-29 21:16:37 -0300

    some ui ajusts.

--- components/SearchInput.js
+++ components/SearchInput.js
@@ -10,7 +10,9 @@ const SearchInput = (props) => {
                         <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
                             <SearchIcon className="w-5 h-5 text-gray-500 dark:text-gray-400"/>
                         </div>
-                        <input type="text" id="tag-search" className="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" placeholder="Search Tags" required/>               
+                        <input type="text" id="tag-search" 
+                        className="outline-none block p-4 pl-10 w-full text-sm text-black/85 rounded-full border border-neutral-300 hover:shadow hover:shadow-neutral-800/20 focus:shadow focus:shadow-neutral-800/20" 
+                        placeholder="Search Tags" required/>               
                     </div>
                     </div>
                 </div>
