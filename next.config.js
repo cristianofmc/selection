002c65fb5d49f34e1cf7c394c647e48213e5d5fa
Cristiano Felipe <cristiano.fmc@hotmail.com>
2022-08-30 00:06:34 -0300

    Revert "[M5-3] Search engine implemented"

--- next.config.js
+++
@@ -1,7 +0,0 @@
-// next.config.js
-module.exports = {
-  images: {
-    domains: ['raw.githubusercontent.com'],
-  },
-}
-
--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,25 @@
+import Link from 'next/link';
 import Head from 'next/head';
 import Hero from '../components/Hero';
-import MenuItem from '../components/MenuItem';
-import Projects from '../components/Projects';
-import SearchInput from '../components/SearchInput';
-import configData from '../lib/config.json'
-import { getUserDetails, getReposDetails,  filterReposByTopics} from '../lib/github-api';
-import { useState } from 'react';
+import { getUserDetails, getReposByTopics} from '../lib/github-api';
 
 function Home(props){
-    const [tagSearch, setTagSearch] = useState([configData.KEY_TAG]);
-
-    const getTagsFromInput = (fullString) => {
-        let tags = fullString.split(';').slice(0, -1).map(x => x.trim());
-        tags.push(configData.KEY_TAG);
-        return tags;
-    };
-
-    const filteredRepos = tagSearch.length > 0
-    ? filterReposByTopics(props.repos, tagSearch) : [];
-
     return(
-        <div className='max-w-7xl mx-auto sm:px-6 md:px-8 mb-4'>
-            <Head><title>{props.user.name}</title></Head>
-            <Hero>{props.user.name}</Hero>
-            <div className="mt-6 sm:mt-10 flex justify-center space-x-6 ">
-                <MenuItem href="">Projects</MenuItem>
-                <MenuItem href="about">About</MenuItem>
-                <MenuItem href="contact">Contact</MenuItem>
+        <div>
+            <Head><title>Cristiano Felipe</title></Head>
+            <Hero></Hero>
+            <div>
+                {props.repos.map((data) => {
+                    return <p className='text-center'>{data.name}</p>
+                })}
             </div>
-            <div className='mt-10'>
-                <SearchInput onChange={e => setTagSearch(getTagsFromInput(e.target.value))}/>
-            </div>
-            <section className="mt-4">
-                <Projects>{filteredRepos}</Projects>
-            </section>
         </div>
     )
 }
 
 export async function getStaticProps(){
-    const userDetails = await getUserDetails();
-    const reposDetails = await getReposDetails();
+    const userDetails = await getUserDetails()
+    const reposDetails = await getReposByTopics()
 
     return {
         props: {
