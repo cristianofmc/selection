3370d9c4b42e26468226462b77b5ab0ca16a5618
cristiano.fmc <cristiano.fmc@hotmail.com>
2022-09-07 20:28:40 -0300

    interface ajusts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,24 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       fontFamily: {
-        ubuntu: ["ubuntu"],
-        roboto: ['Roboto', 'arial','sans-serif'],
+        ubuntu: ['ubuntu'],
+        roboto: ['oboto', 'arial','sans-serif'],
       },
       colors: {
         'hero-black': '#202124',
       },
+      padding: {
+        '5px': '5px',
+        '3px': '3px',
+      },
+      letterSpacing: {
+        widestPlus: '.01785714em',
+      }
     },
   },
   plugins: [],
