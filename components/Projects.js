68f7a5c9ff6d1536daca095ece20f6499a203657
cristiano.fmc <cristiano.fmc@hotmail.com>
2022-09-14 21:53:26 -0300

    Added support to screen readers

--- components/Projects.js
+++ components/Projects.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import CoverRender  from './CoverRender';
 import Modal from './Modal';
 import Topic from './Topic';
+import NoResults from './NoResults';
 
 const Projects = (props) => {
 
@@ -19,21 +20,30 @@ const Projects = (props) => {
         setModalOn(false);
     }
 
+    if(!props.children.length) return <NoResults/>
+
     return (
         <div id="projects" className="py-3">
             <ul key="1" className="mx-2 md:mx-4 grid  grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-2 md:gap-8">
                 {
                     props.children.map((data, index) => {
-                        return <li id={data.id} key={data.id} className="rounded-lg border border-neutral-300">
+                        return <li focus-within id={data.id} key={data.id} className="rounded-lg border border-neutral-300">
                             <div className="overflow-hidden cursor-pointer" 
                             onClick={() => {setModalData(data); handleOnOpen();} }>
                                 <CoverRender width="600" height="400">{data}</CoverRender>
                             </div>
 
                             <div className="px-4 pt-3">
-                                <h1 className="font-bold truncate">{data.name}</h1>
+                                <div className="overflow-hidden cursor-pointer" 
+                                onClick={() => {setModalData(data); handleOnOpen();} }>
+                                    <button className="font-bold truncate">{data.name}</button>
+                                </div>
+                            </div>
+
+                            <div className="px-4">
                                 <p className='pt-px'>{data.description}</p>
                             </div>
+
                             <div className="px-2.5 py-5px overflow-hidden">
                                 <Topic close={false}>{data.topics}</Topic>
                             </div>
