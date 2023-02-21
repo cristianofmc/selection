cc7ef3bdddc7cb7d25f419a6ddc05dc8620687df
cristianofmc <cristiano.fmc@hotmail.com>
2023-02-21 14:25:03 -0300

    remove image conditional

--- components/CoverRender.js
+++ components/CoverRender.js
@@ -2,14 +2,12 @@ import Image from 'next/image'
 import { useEffect, useState } from 'react';
 
 const CoverRender = (props) => {
-    const [displayimage, setDisplayimage] = useState(true);
-
     let cover_url = props.children.html_url.replace("https://github.com/", "https://raw.githubusercontent.com/");
     cover_url += `/${props.children.default_branch}/_docs/cover.webp`;
 
     return (
-        <div className={`overflow-hidden ${displayimage ? `block` : `hidden`}`}>
-            <Image alt="" className="relative w-full  object-cover rounded-t-md"  src={cover_url} width={props.width} height={props.height} onError={() => setDisplayimage(false)}></Image>
+        <div className={'overflow-hidden'}>
+            <Image alt="" className="relative w-full object-cover rounded-t-md"  src={cover_url} width={props.width} height={props.height}></Image>
         </div>
     );
 };
