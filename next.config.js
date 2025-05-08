5b5dc07605676053e4bde770e9c0165404d5c60b
cristianofmc <cristiano.fmc@hotmail.com>
2025-05-08 00:01:21 -0300

    removing iL8n and using array on filterReposByTopics

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 module.exports = {
   images: {
     domains: ['avatars.githubusercontent.com', 'raw.githubusercontent.com'],
-  },
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
+  }
 };
